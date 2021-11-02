52c688503e373cc1171924caabb2cab6e9fe1a2d
Koray Aksoy <burakkorayaksoy@hotmail.com>
2021-11-02 23:46:31 +0300

    empty bottom sheet successfully added

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React,{useRef, useMemo} from 'react';
 import { StyleSheet, Text, View, FlatList, SafeAreaView} from 'react-native';
 import ListItem from './components/ListItem';
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
 
 import { SAMPLE_DATA } from './assets/data/sampleData';
 
@@ -13,24 +17,45 @@ const ListHeader = () => (
     </>
 )
 export default function App() {
+
+  const bottomSheetModalRef = useRef(null);
+
+  const snapPoints = useMemo(() => ['50%'], []);
+
+  const openModal = () => {
+    bottomSheetModalRef.current.present();
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      
-    <FlatList 
-      keyExtractor={(item) => item.id}
-      data={SAMPLE_DATA}
-      renderItem={({item}) => (
-        <ListItem
-        name={item.name}
-        symbol={item.symbol}
-        currentPrice={item.current_price}
-        priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
-        logoUrl={item.image}
+    <BottomSheetModalProvider>
+      <SafeAreaView style={styles.container}>   
+        <FlatList 
+          keyExtractor={(item) => item.id}
+          data={SAMPLE_DATA}
+          renderItem={({item}) => (
+            <ListItem
+            name={item.name}
+            symbol={item.symbol}
+            currentPrice={item.current_price}
+            priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
+            logoUrl={item.image}
+            onPress={() => openModal()}
+            />
+          )}
+          ListHeaderComponent={<ListHeader/>}
         />
-      )}
-      ListHeaderComponent={<ListHeader/>}
-    />
-    </SafeAreaView>
+      </SafeAreaView>
+
+      <BottomSheetModal 
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+      >
+          <View style={styles.contentContainer}>
+            <Text>Awesome 🎉</Text>
+          </View>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
   
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
 
-const ListItem = ({name, symbol, currentPrice, priceChangePercentage7d, logoUrl}) => {
+const ListItem = ({name, symbol, currentPrice, priceChangePercentage7d, logoUrl, onPress}) => {
   
 const priceChangeColor = priceChangePercentage7d > 0 ? '#34C759':'#FF3B30';    
     return (
-        <TouchableOpacity>
+        <TouchableOpacity onPress = {onPress}>
             <View style={styles.itemWrapper}>
                 {/* Left side */}
                 <View style={styles.leftWrapper}>
